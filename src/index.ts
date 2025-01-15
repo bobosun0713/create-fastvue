7f363871b58c87c93e75fbe1a22ff0407b7d6bb0
bobosun0713 <chiabo.sun@gmail.com>
2025-01-15 22:33:52 +0800

    fix: fix error handler creation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
-#! /usr/bin/env node
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath, URL } from "node:url";
 
-import { askProjectName, askTemplate } from "./utils/actions";
-import create from "./utils/create";
+import { askOverwrite, askProjectName, askTemplate } from "./utils/actions";
+import { create, doneMessage } from "./utils/create";
 
-async function init(): Promise<void> {
-  const projectName = await askProjectName();
-  const template = await askTemplate();
-
-  void create(projectName, template);
+async function checkIfOverwrite(targetDirectory: string): Promise<boolean> {
+  if (fs.existsSync(targetDirectory)) return askOverwrite();
+  return false;
 }
 
-void init().catch((error: unknown) => {
-  if (error instanceof Error) console.error(error.message);
-});
+async function getTemplateDirectory(): Promise<string> {
+  const templateType = await askTemplate();
+  return fileURLToPath(new URL(`../src/template/${templateType}`, import.meta.url));
+}
+
+async function init(): Promise<void> {
+  try {
+    const cwd = process.cwd();
+    const projectName = await askProjectName();
+    const targetDirectory = path.join(cwd, projectName);
+
+    const isOverwrite = await checkIfOverwrite(targetDirectory);
+    if (!isOverwrite) return;
+
+    const templateDirectory = await getTemplateDirectory();
+
+    await create(targetDirectory, templateDirectory, isOverwrite);
+
+    doneMessage(projectName);
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error("An unknown error occurred.");
+  }
+}
+
+void init();
--- src/utils/create.ts
+++ src/utils/create.ts
@@ -1,9 +1,6 @@
 import * as fs from "node:fs";
-import * as path from "node:path";
-import { fileURLToPath, URL } from "node:url";
 
 import { LOGO, VERSION } from "../constants";
-import { askOverwrite } from "./actions";
 import copyDirectory from "./copyDirectory";
 
 interface PkgInfo {
@@ -13,15 +10,18 @@ interface PkgInfo {
 
 function pkgFromUserAgent(userAgent: string | undefined): PkgInfo | undefined {
   if (!userAgent) return undefined;
+
   const pkgSpec = userAgent.split(" ")[0];
   const pkgSpecArr = pkgSpec.split("/");
+
   return {
     name: pkgSpecArr[0],
     version: pkgSpecArr[1]
   };
 }
 
-function doneMessage(projectName: string, pkgInfo: PkgInfo | undefined): void {
+export function doneMessage(projectName: string): void {
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
   const { name } = pkgInfo ?? {};
 
   console.log(`\n${VERSION}
@@ -34,29 +34,11 @@ Successfully. Now run:\n
   `);
 }
 
-async function create(projectName: string, projectType: string): Promise<void> {
-  const cwd = process.cwd();
-
-  const targetDirectory = path.join(cwd, projectName);
-  const templateDirectory = fileURLToPath(new URL(`../src/template/${projectType}`, import.meta.url));
-  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
-
+export async function create(targetDirectory: string, templateDirectory: string, overwrite: boolean): Promise<void> {
   try {
-    // Check if the directory exists
-    if (fs.existsSync(targetDirectory)) {
-      const overwrite = await askOverwrite();
-      if (!overwrite) return;
-
-      await fs.promises.rm(targetDirectory, { recursive: true, force: true });
-      await copyDirectory(templateDirectory, targetDirectory);
-    } else {
-      await copyDirectory(templateDirectory, targetDirectory);
-    }
-
-    doneMessage(projectName, pkgInfo);
+    if (overwrite) await fs.promises.rm(targetDirectory, { recursive: true, force: true });
+    await copyDirectory(templateDirectory, targetDirectory);
   } catch (error) {
     throw new Error(`🔴 Error creating project: ${error as string}`);
   }
 }
-
-export default create;
